0bc963d67267852d70bd1b2a05cf30e3e1ed3889
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-01-21 12:11:32 +0000

    🦀1️⃣ - lqwk/w1

---
+++ work1/target/debug/deps/work1-fe621429fd4882b8.d
@@ -0,0 +1,5 @@
+/workspaces/sn-rust-progz/work1/target/debug/deps/work1-fe621429fd4882b8.rmeta: src/main.rs
+
+/workspaces/sn-rust-progz/work1/target/debug/deps/work1-fe621429fd4882b8.d: src/main.rs
+
+src/main.rs:

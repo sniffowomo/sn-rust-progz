59b770cef14799a856166b28555fd42df75142e5
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-01-21 12:15:56 +0000

    🔮 - Runign the additions

---
+++ work1/target/debug/deps/work1-8f235dee69900c27.d
@@ -0,0 +1,5 @@
+/workspaces/sn-rust-progz/work1/target/debug/deps/work1-8f235dee69900c27: src/main.rs
+
+/workspaces/sn-rust-progz/work1/target/debug/deps/work1-8f235dee69900c27.d: src/main.rs
+
+src/main.rs:
